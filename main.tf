1cb1127626af026d1bf1f8595eeea8d2af319ec3
wcollins <william.levi.collins@gmail.com>
2023-05-10 12:24:34 -0400

    Update

--- main.tf
+++ main.tf
@@ -1,6 +1,4 @@
 resource "aws_vpc" "this" {
-  provider    = aws.us-east-1
-
   cidr_block  = "10.0.0.0/16"
 
   tags = {
@@ -10,8 +8,6 @@ resource "aws_vpc" "this" {
 }
 
 resource "aws_subnet" "this" {
-  provider    = aws.us-east-1
-
   vpc_id      = aws_vpc.this.id
   cidr_block  = "10.0.1.0/24"
 
--- provider.tf
+++ provider.tf
@@ -3,8 +3,3 @@ provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
 }
-
-provider "aws" {
-  alias      = "us-east-2"
-  region     = "us-east-2"
-}
