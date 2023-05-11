ef1b7ca923229248dad66a9f401bf8216cb2c151
wcollins <william.levi.collins@gmail.com>
2023-05-11 10:52:47 -0400

    Update

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "this" {
-  cidr_block  = "10.0.0.0/16"
+  cidr_block  = "10.5.0.0/16"
 
   tags = {
     Name = "dev"
@@ -9,7 +9,7 @@ resource "aws_vpc" "this" {
 
 resource "aws_subnet" "this" {
   vpc_id      = aws_vpc.this.id
-  cidr_block  = "10.0.1.0/24"
+  cidr_block  = "10.5.1.0/24"
 
   tags = {
     Name = "app"
