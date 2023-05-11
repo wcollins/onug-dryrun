13d46642e6661058ebe2def91ca08b54fbc8d4aa
wcollins <william.levi.collins@gmail.com>
2023-05-11 11:41:05 -0400

    Add security group

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "aws_internet_gateway" "this" {
 
 }
 
-resource "aws_route_table" "public" {
+resource "aws_route_table" "this" {
   vpc_id = aws_vpc.this.id
 
   route {
@@ -43,4 +43,29 @@ resource "aws_route_table" "public" {
 resource "aws_route_table_association" "public" {
   subnet_id       = aws_subnet.this.id
   route_table_id  = aws_route_table.this.id
+}
+
+resource "aws_security_group" "this" {
+  vpc_id      = aws_vpc.this.id
+  name        = "great-security-practices"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags    = {
+    Name  = "great-security-practices"
+  }
+
 }
